fd0934e4fb0c2a7987cf0aed3a25dd041cca1e3a
parseltongist <>
2022-03-18 19:39:54 +0100

    stage 3, post to DB and get from db features implemented

--- Web Calendar/task/app.py
+++ Web Calendar/task/app.py
@@ -1,22 +1,79 @@
+import datetime
 import sys
 
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, inputs
+from flask_restful import Resource, Api, reqparse, inputs, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
+
+
+TABLE_NAME = 'events'
+
+parser = reqparse.RequestParser()
+
+parser.add_argument(
+    'date',
+    type=inputs.date,
+    help="The event date with the correct format is required! "
+         "The correct format is YYYY-MM-DD!",
+    required=True
+)
+
+parser.add_argument(
+    'event',
+    type=str,
+    help="The event name is required!",
+    required=True
+)
+
 
 app = Flask(__name__)
+# SQLALCHEMY_DATABASE_URI is a DB configuration key
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{TABLE_NAME}.sqlite'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# write your code here
+db = SQLAlchemy(app)  # initialize database
 api = Api(app)
+db.create_all()  # save the table in the database
 
 
-class EventResource(Resource):
+resource_fields = {
+    "id": fields.Integer,
+    "event": fields.String,
+    "date": fields.String,
+}
+
+
+class Event(db.Model):
+    __tablename__ = TABLE_NAME
+
+    id = db.Column(db.Integer, primary_key=True)
+    event = db.Column(db.String(80), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+
+
+class AllEventsResource(Resource):
+
     @staticmethod
+    @marshal_with(resource_fields)
     def get():
-        return {"data": "There are no events for today!"}
+        all_events = Event.query.all()
+        if not all_events:
+            return {"data": "There are no events for today!"}
+
+        return all_events
 
     @staticmethod
     def post():
+        def add_to_db():
+            event = Event(event=args['event'],
+                          date=args['date'],
+                          )
+            db.session.add(event)
+            db.session.commit()
+
         args = parser.parse_args()
+        add_to_db()
+
         response = {
             "message": "The event has been added!",
             "event": args['event'],
@@ -26,28 +83,21 @@ class EventResource(Resource):
         return response
 
 
-def set_arguments():
-    parser.add_argument(
-        'date',
-        type=inputs.date,
-        help="The event date with the correct format is required! The correct format is YYYY-MM-DD!",
-        required=True
-    )
+class EventResourceToday(AllEventsResource):
+    @staticmethod
+    @marshal_with(resource_fields)
+    def get():
+        todays_events = Event.query.filter(Event.date == datetime.date.today()).all()
+        if not todays_events:
+            return {"data": "There are no events for today!"}
+        return todays_events
 
-    parser.add_argument(
-        'event',
-        type=str,
-        help="The event name is required!",
-        required=True
-    )
 
+api.add_resource(EventResourceToday, '/event/today')
+api.add_resource(AllEventsResource, '/event')
 
 # do not change the way you run the program
 if __name__ == '__main__':
-    parser = reqparse.RequestParser()
-    set_arguments()
-    api.add_resource(EventResource, '/event/')
-
     if len(sys.argv) > 1:
         arg_host, arg_port = sys.argv[1].split(':')
         app.run(host=arg_host, port=arg_port)
--- Web Calendar/task/tests.py
+++ Web Calendar/task/tests.py
@@ -1,8 +1,12 @@
-from hstest import FlaskTest, dynamic_test
+import os
+
+from hstest import FlaskTest, dynamic_test, WrongAnswer
 
 from test.base import (
     test_correct_request,
-    test_bad_request
+    test_bad_request,
+    test_get_events,
+    test_today_events
 )
 
 
@@ -11,8 +15,18 @@ class ServerTest(FlaskTest):
 
     funcs = [
         test_correct_request,
-        test_bad_request
+        test_bad_request,
+        test_get_events,
+        test_today_events
     ]
+
+    def generate(self):
+        if os.path.exists('event.db'):
+            try:
+                os.remove('event.db')
+            except Exception:
+                raise WrongAnswer("Can't delete the database file before starting the tests!")
+        return []
 
     @dynamic_test(data=funcs)
     def test_correct_request(self, func):
